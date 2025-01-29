d5662f0d280f0ff2ff20c1e126d018b12a01d9eb
nakzyu <nakzyu@gmail.com>
2025-01-29 17:54:36 +0900

    feat: add tailwind configs

--- apps/bender/src/app/app.tsx
+++ apps/bender/src/app/app.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useCallback } from "react";
 import { createRoot } from "react-dom/client";
 import { FigmaMessage } from "../messages";
-import { HiOutlineCog } from "react-icons/hi";
-
 import { Button } from "@repo/ui";
 import "@repo/ui/styles.css";
 
@@ -31,21 +29,16 @@ function App() {
   }, [figmaMessageListener]);
 
   return (
-    <div className="p-4">
+    <div className="container w-full h-full px-4">
       <div className="w-full flex flex-col items-center mb-5">
-        <HiOutlineCog size={50} className="animate-spin" />
         <h1 className="text-xl">Figma React + Tailwind + Webpack Starter</h1>
         <h2 className="text-md text-gray-400">Plugin starter</h2>
       </div>
 
       <div className="mt-6 w-full flex justify-center">
-        <button
-          onClick={onSendMessage}
-          className="text-7xl text-red-700 inline-flex px-20"
-        >
-          sda
-        </button>
-        <Button variant="destructive">@@@@@</Button>
+        <Button onClick={onSendMessage} variant="destructive">
+          @@@@@
+        </Button>
       </div>
     </div>
   );

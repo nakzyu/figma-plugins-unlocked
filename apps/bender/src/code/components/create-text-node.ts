a50819cc2de2dabdf3dbee73af9a333d51d11971
nakzyu <nakzyu@gmail.com>
2025-02-07 21:20:30 +0900

    feat: create group in the center of screen

--- apps/bender/src/code/components/create-text-node.ts
+++ apps/bender/src/code/components/create-text-node.ts
@@ -17,9 +17,23 @@ export const createTextNode = async (message: TO_CODE_CREATE_TEXT_NODE) => {
     textNode.x = data.x;
     textNode.y = data.y;
     textNode.rotation = -data.rotation;
-
     return textNode;
   });
 
-  figma.group(textNodes, figma.currentPage);
+  const group = figma.group(textNodes, figma.currentPage);
+
+  const {
+    x: vpX,
+    y: vpY,
+    width: vpWidth,
+    height: vpHeight,
+  } = figma.viewport.bounds;
+  const viewportCenterX = vpX + vpWidth / 2;
+  const viewportCenterY = vpY + vpHeight / 2;
+
+  const groupCenterX = group.x + group.width / 2;
+  const groupCenterY = group.y + group.height / 2;
+
+  group.x += viewportCenterX - groupCenterX;
+  group.y += viewportCenterY - groupCenterY;
 };
